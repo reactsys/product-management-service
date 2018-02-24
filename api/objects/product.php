1f7d0ec72ca45deadacd9e9444a83a89348fa842
paulb150 <paul.broekman@gmail.com>
2018-02-24 08:10:29 +0200

    Add Product class

---
+++ api/objects/product.php
@@ -0,0 +1,18 @@
+<?php
+class Product{
+
+    private $conn;
+    private $table_name = "products";
+
+    public $id;
+    public $name;
+    public $description;
+    public $price;
+    public $category_id;
+    public $category_name;
+    public $created;
+
+    public function __constrcut($db){
+        $this->conn = $db;
+    }
+}
