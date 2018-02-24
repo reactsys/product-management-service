f643f2b7d32fe6e820429643c383f180d02d9db9
paulb150 <paul.broekman@gmail.com>
2018-02-24 07:58:23 +0200

    Apply fixes database scripts

--- database/scripts.sql
+++ database/scripts.sql
@@ -1,3 +1,5 @@
+DROP DATABASE customer_management_service;
+
 CREATE DATABASE customer_management_service;
 
 USE customer_management_service;
@@ -11,7 +13,7 @@ CREATE TABLE IF NOT EXISTS `categories` (
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=19 ;
 
-NSERT INTO `categories` (`id`, `name`, `description`, `created`, `modified`) VALUES
+INSERT INTO `categories` (`id`, `name`, `description`, `created`, `modified`) VALUES
 (1, 'Fashion', 'Category for anything related to fashion.', '2014-06-01 00:35:07', '2014-05-30 17:34:33'),
 (2, 'Electronics', 'Gadgets, drones and more.', '2014-06-01 00:35:07', '2014-05-30 17:34:33'),
 (3, 'Motors', 'Motor sports and more', '2014-06-01 00:35:07', '2014-05-30 17:34:54'),
